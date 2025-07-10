f7feb43a66cf9513963d0d2a98f82c28f770db8b
darkpark02 <jparke62@emich.edu>
2025-07-10 14:32:07 -0400

    User 2 commit

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 System.out.println("Hello world");
 System.out.println("Commit #2");
+System.out.println("Programmer 2 did this.");
 	}
 }
